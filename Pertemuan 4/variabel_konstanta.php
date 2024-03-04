144ece9acdf094e0e589cccbe4a89b1f81690550
IvanYoda <160442141+IvanYoda@users.noreply.github.com>
2024-03-04 13:08:44 +0700

    prak 1

---
+++ Pertemuan 4/variabel_konstanta.php
@@ -0,0 +1,21 @@
+<?php
+// Praktikum 1
+
+// Praktikum 1 langkag 2
+    $angka1 = 10;
+    $angka2 = 5;
+    $hasil = $angka1 + $angka2;
+    echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil. <br>";
+
+// Praktikum 1 langkag 4
+    $benar = true;
+    $salah = false;
+    echo "Variabel benar: $benar, Variabel salah: $salah <br>";
+
+// Praktikum 1 langkag 6
+    //Mendefinisikan konstanta untuk nilai tetap
+    define("NAMA_SITUS", "WebsiteKu.com");
+    define("TAHUN_PENDIRIAN", 2023);
+    echo "Selamat datang di " . NAMA_SITUS . ", situs yang
+    didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
+?>
